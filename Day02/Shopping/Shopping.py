be7a2aa3b16bd514ac885fdfaafdd775b6bed3c5
zhangyh <zhangyh@mail.open.com.cn>
2017-11-08 19:40:48 +0800

    添加商品列表

--- Day02/Shopping/Shopping.py
+++ Day02/Shopping/Shopping.py
@@ -14,33 +14,41 @@
 import json
 # 用户信息文件
 userFileName='users.json'
-# 用户信息数组
-users=[]
+productFileName='products.json'
 # 读取用户信息文件
 def ReadUserFile():
     with open(userFileName, 'r', encoding='utf-8') as f:
         users = json.load(f)
+    return users
 # 写入用户信息文件
-def WriteUserFile():
+def WriteUserFile(users):
     with open(userFileName,'w',encoding='utf-8') as f1:
         json.dump(users, f1, ensure_ascii=False)
 #添加用户信息
 def InsertUser(name,pwd):
-    ReadUserFile()
+    users=ReadUserFile()
     userInfo = {'name': name, 'pwd': pwd, 'amount': 0}
     users.append(userInfo)
-    WriteUserFile()
+    WriteUserFile(users)
 # 修改用户工资
 def UpdateUserAmount(name,amount):
-    ReadUserFile()
+    users = ReadUserFile()
     for u_dict in users:
         if u_dict["name"] == name:
             u_dict["amount"] = amount
-            WriteUserFile()
+            WriteUserFile(users)
             break
+# 查询用户余额
+def GetUserAmount(name):
+    users = ReadUserFile()
+    for u_dict in users:
+        if u_dict["name"] == name:
+            amount = u_dict["amount"]
+            break
+    return amount
 # 检查用户是否存在
 def IsExistUserName(name):
-    ReadUserFile()
+    users = ReadUserFile()
     isExist = False
     for u_dict in users:
         if u_dict["name"] == name:
@@ -48,31 +56,63 @@ def IsExistUserName(name):
     return isExist
 # 检查用户名和密码是否正确
 def CheckUserNamePwd(name,pwd):
-    ReadUserFile()
+    users = ReadUserFile()
     isExist = False
     for u_dict in users:
         if u_dict["name"] == name and u_dict["pwd"] == pwd:
             isExist = True
     return isExist
+# 获取用户消费记录
+
+# 获取商品列表
+def ReadProductFile():
+    with open(productFileName, 'r', encoding='utf-8') as f2:
+        products = json.load(f2)
+    return products
+
+
+
 while True:
     str_welcome = "欢迎进入购物系统"
     print(str_welcome.center(30,'*'))
-    userName=input("请输入用户名：")
+    userName=input("请输入用户名：").strip()
     userPwd=input("请输入密码：")
+    isLogin=False
     if IsExistUserName(userName):
         while not CheckUserNamePwd(userName,userPwd):
+            print("密码错误，请重新输入！")
             userPwd = input("请输入密码：")
         else:
-            print("登录成功")
+            isLogin=True
     else:
         InsertUser(userName,userPwd)
         while True:
-            amount = input("请输入工资：")
-            if not amount.isdigit():
+            userAmount = input("请输入工资：")
+            if not userAmount.isdigit():
                 print("工资必须为数字,请重新输入！")
                 continue
             else:
-                print("合法")
+                UpdateUserAmount(userName,userAmount)
+                isLogin = True
+                break
+    if isLogin:
+        amount = GetUserAmount(userName)
+        print("您的账户余额：\033[31;1m{_amount}\033[0m".format(_amount=amount))
+        # 是否显示消费记录
+        isShow=input("是否显示消费记录(Y/N)：").strip()
+        if isShow=="Y":
+            # 显示消费记录
+            print("显示消费记录")
+        else:
+            # 显示商品列表
+            str_product = "商品列表"
+            print(str_product.center(40, '*'))
+            print('%-5s %-15s %-10s' % ('编号', '商品名称', '商品价格(元)'))
+            products = ReadProductFile()
+            for index, item in enumerate(products):
+                print('%-8d %-20s %-10d' % (index + 1, item['name'], item['price']))
+
+
 
 
 
