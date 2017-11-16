bfe6ee7b1d1bc0123fe7be0a13b2b3f9155f91e0
zhangyh <zhangyh@mail.open.com.cn>
2017-11-16 18:18:15 +0800

    添加员工信息数据

---
+++ Day04/EmployeeInfoTable/1.py
@@ -0,0 +1,17 @@
+# -------------------------------
+# Task Name：
+# Description ：
+# Author ： Danny
+# date： 2017/11/16
+# -------------------------------
+
+
+test_dict={
+    "key1":"1",
+    "key2":"2"
+}
+
+if "key1" in test_dict:
+    print(True)
+if "key11" in test_dict:
+    print(False)
--- Day04/EmployeeInfoTable/Employee.py
+++ Day04/EmployeeInfoTable/Employee.py
@@ -17,64 +17,121 @@
 # -------------------------------
 # sql 解析
 def SqlParse(sql):
-    parse_commond={
-        "insert":InsertParse,
-        "delete":DeleteParse,
-        "update":UpdateParse,
-        "select":SelectParse
+    parse_commond = {
+        "insert": InsertParse,
+        "delete": DeleteParse,
+        "update": UpdateParse,
+        "select": SelectParse
     }
-    sql_spl=sql.split(' ')
-    sql_commond=sql_spl[0]
-    sql_dict=''
+    sql_split = sql.split(' ')
+    sql_commond = sql_split[0]
+    sql_dict = ''
     if sql_commond in parse_commond:
-        sql_dict=parse_commond[sql_commond](sql_spl)
+        sql_dict = parse_commond[sql_commond](sql_split)
     else:
         print("输入错误，请重新输入！")
     return sql_dict
 
+
 def InsertParse(sql):
     pass
+
+
 def DeleteParse(sql):
     pass
+
+
 def UpdateParse(sql):
     pass
+
+
 def SelectParse(sql_spl):
-    sql_dict={
-        "commond":Select,
-        "select":[],
-        "from":[],
-        "where":[],
-        "limit":[]
+    sql_dict = {
+        "commond": select,  # 命令
+        "select": [],  # 字段
+        "from": [],  # 表名
+        "where": [],  # 过滤条件
+        "limit": []  # 记录数
     }
-    sql_dic = CommondParse(sql_dict,sql_spl)
+    sql_dic = CommondParse(sql_dict, sql_spl)
     return sql_dic
 
-def CommondParse(sql_dict,sql_spl):
-    pass
+
+# select * from staff_table where enroll_date like "2013"
+def CommondParse(sql_dict, sql_spl):
+    # 设置标志位
+    tag = False
+    str_char = ''
+    item_key = ''
+    for spl in sql_spl:
+        if tag and spl in sql_dict:
+            sql_dict[item_key]=str_char
+            tag=False
+            # sql_dict[item_key] = str_char
+            # item_key = spl
+            # tag = True
+            # str_char = ''
+            # continue
+         if not tag and spl in sql_dict:
+                item_key = spl
+                tag = True
+                continue
+         if tag:
+                str_char += spl
+    else:
+        sql_dict[item_key] = str_char
+    return sql_dict
+
+
 # sql 执行
 def sql_Execute(sql_dic):
     pass
+
+
 def Insert(sql_dic):
     pass
+
+
 def Delete(sql_dic):
     pass
+
+
 def Update(sql_dic):
     pass
+
+
 def Select(sql_dic):
     pass
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     while True:
-        sql=input("sql>").strip()
-        if sql=="exit": break
-        if len(sql)==0: continue
+        welcome_str = '''
+                                              欢迎来到员工信息查询程序
+        ------------------------------------------------------------------------------------------------------
+        操作语法说明：
+        添加：INSERT INTO 表名称 VALUES 值1, 值2,....
+        删除：DELETE —> 输入欲删除的条目编号
+        修改：UPDATE 表名称 SET 列名称 = 新值 WHERE 列名称 = 某值
+        查询：SELECT * FROM 表名称 WHERE 列名称 = 某值
+              SELECT * FROM 表名称 WHERE 列名称 LIKE 某值
+              SELECT 列名称1,列名称2... FROM 表名称 WHERE 列名称 = 某值
+        ------------------------------------------------------------------------------------------------------
+            '''
+        print(welcome_str)
+        input_sql = input('请输入SQL语句(或者输入q退出)：\nSQL>').strip()
+        if input_sql.lower() == 'q':
+            print('感谢使用模拟SQL程序，欢迎再次使用！再见！')
+            exit()
+        else:
+            if len(input_sql) == 0:
+                continue
         # sql解析
-        sql_dict=SqlParse(sql)
-        if len(sql_dict)==0: continue
+        sql_dict = SqlParse(input_sql)
+        print(sql_dict)
+        # if len(sql_dict)==0: continue
 
 
         # sql执行
 
         # res = sql_Execute(sql_dict)
-
-
